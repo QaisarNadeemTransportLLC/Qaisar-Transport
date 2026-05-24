e6c9fa182a2f811bb1374e87ac8e09b6de8d7541
QaisarNadeemTransportLLC <tiktokehsansohnajatt@gmail.com>
2026-05-24 17:03:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.markdown("""
     
     /* Buttons ki styling */
     div.stButton > button {
-        width: 200px !important;
+        width: 230px !important;
         height: 55px !important;  /* Height fix */
         background-color: #FF9559 !important;
         color: #ffffff !important;
